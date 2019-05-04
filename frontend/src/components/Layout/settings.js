f91c0d122efe662a882a17fff34dff40316a78e6
Nicole Campbell <nscampbell303@gmail.com>
2019-05-04 12:52:03 -0400

    modified

--- frontend/src/components/Layout/settings.js
+++ frontend/src/components/Layout/settings.js
@@ -1,3 +1,3 @@
 export const clientId = 'b40a880e696b433981d9888e1f9c9ab3'
 
-export const redirectUri = 'http://localhost:3000/callback/'
+export const redirectUri = 'http://3.89.148.114:3000/callback/'

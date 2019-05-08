85c442253bc2c7f89f822a6030d67f833d25260f
Nicole Campbell <nscampbell303@gmail.com>
2019-05-08 14:09:00 -0400

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.use(cors());
 app.use(methodOverride('_method')); 
 
